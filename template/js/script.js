bbec6ab44fb53b53ece4a0a51c44f465474cc872
ysnbhb <yassine.bahbib@usmba.ac.ma>
2024-12-05 15:14:18 +0100

    sing up

--- template/js/script.js
+++ template/js/script.js
@@ -23,19 +23,20 @@ function confermetPas() {
   if (password.value === "") {
     passwdlabel.innerHTML = "invalid password";
     passwdlabel.style.color = "red";
-    passwdlabel.style.fontSize = "15px";
+    passwdlabel.style.fontSize = "12px";
     passwdlabel.style.marginLeft = "8px";
     password.style.border = "1.8px solid red";
+    password.focus();
     return true;
   } else {
     passwdlabel.innerHTML = "";
     password.style.border = "1px solid #ccc";
   }
   if (password.value !== confpassword.value) {
-    confpasswordlabel.innerHTML = "please confirm Password";
     confpasswordlabel.style.color = "red";
-    confpasswordlabel.style.fontSize = "15px";
+    confpasswordlabel.style.fontSize = "12px";
     confpasswordlabel.style.marginLeft = "8px";
+    confpasswordlabel.innerHTML = "please confirm Password";
     confpassword.style.border = "1.8px solid red";
     confpassword.focus();
     return true;
@@ -51,20 +52,32 @@ function runButton(idButton) {
   const userName = document.getElementById("user_name");
   const email = document.getElementById("email");
   const password = document.getElementById("password");
+  const labe_user = document.getElementById("user_label");
+  const passwd_lable = document.getElementById("email_label");
   button.addEventListener("click", (event) => {
     event.preventDefault();
+    labe_user.style.color = "red";
+    labe_user.style.marginLeft = "8px";
+    labe_user.style.fontSize = "12px";
+    passwd_lable.style.color = "red";
+    passwd_lable.style.marginLeft = "8px";
+    passwd_lable.style.fontSize = "12px";
     if (userName.value === "") {
+      labe_user.innerHTML = "Invalid input";
       userName.focus();
       userName.style.border = "1.8px solid red";
       return;
     } else {
+      labe_user.innerHTML = "";
       userName.style.border = "1px solid #ccc";
     }
     if (!validator.isEmail(email.value)) {
+      passwd_lable.innerHTML = "Invalid email form";
       email.focus();
       email.style.border = "1.8px solid red";
       return;
     } else {
+      passwd_lable.innerHTML = "";
       email.style.border = "1px solid #ccc";
     }
     if (confermetPas()) {
