daa3cca7e43d7d4b665b7f48cc3a69f9f02266b7
yasine bahbib <yassine.bahbib@usmba.ac.ma>
2024-12-17 19:00:48 +0100

    server home

--- server/page.go
+++ server/page.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"html/template"
 	"net/http"
 
 	"forum/utils"
@@ -11,4 +12,14 @@ func Page(w http.ResponseWriter, r *http.Request) {
 		utils.ErrorHandler(w, http.StatusNotFound, "Page not Fount", "The page you are looking for is not available!", nil)
 		return
 	}
+	if r.Method != http.MethodGet {
+		utils.ErrorHandler(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed), "this Page doesn't support your Method", nil)
+		return
+	}
+	tmp, err := template.ParseFiles("veiw/home.html")
+	if err != nil {
+		utils.ErrorHandler(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError), "sorry but there are Error in server try next time", nil)
+		return
+	}
+	tmp.Execute(w, nil)
 }
